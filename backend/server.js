0faa36c4c980c74fa7aaf545b6d8c0f57658cd25
Bryan <btlwainwright@gmail.com>
2022-07-13 01:00:26 +0200

    Finish backend

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const asyncHandler = require("express-async-handler");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.json());
+app.use(cors());
 mongoose.connect("mongodb://localhost:27017/notesDB");
 
 const noteSchema = new mongoose.Schema({
@@ -15,11 +18,35 @@ const noteSchema = new mongoose.Schema({
     date: String
 })
 
-const Note = mongoose.Model("Note", noteSchema);
+const Note = mongoose.model("Note", noteSchema);
 
-app.get("/", async (req, res) => {
-    res.json({message: "Hi"});
-});
+app.get("/", asyncHandler(async (req, res) => {
+    Note.find({}, (err, foundItems) => {
+
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(foundItems);
+        }
+    });
+}));
+
+app.post("/", asyncHandler(async (req, res) => {
+    const newNote = new Note(req.body);
+    newNote.save();
+}));
+
+app.delete("/:ID", asyncHandler(async (req, res) => {
+    const deletedNoteId = req.body.ID;
+
+    Note.findByIdAndDelete(deletedNoteId, (err) => {
+
+        if (err) {
+            console.log(err);
+        }
+    });
+}));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import { Collapse } from "@mui/material";
 import Header from "./Header";
@@ -10,19 +10,38 @@ const App = () => {
     const [notes, setNotes] = useState([]);
     const [isExpanded, setIsExpanded] = useState(false);
 
-    const addNote = (newNote) => {
+    const addNote = async (newNote) => {
+
+        axios.post("http://localhost:5000", newNote)
+        .then(response => {
+            // console.log(response);
+        })
         setNotes(prevNotes => {
             return [...prevNotes, newNote]
+        });
+    }
+
+    const getNotes = async () => {
+        axios.get("http://localhost:5000").then(response => {
+            setNotes(response.data);
         })
     }
 
-    axios.get()
-
-    const deleteNote = (id) => {
+    useEffect(() => {
+        getNotes();
+    }, []);
+    
+    const deleteNote = async (id) => {
         setNotes(prevNotes => {
             return prevNotes.filter((item, index) => {
                 return index !== id;
             });
+        });
+        let noteId = notes[id]._id;
+        axios.delete(`http://localhost:5000/ID`, {data: {
+            ID: noteId
+        }}).then(response => {
+            console.log(response.data);
         });
     }
 
